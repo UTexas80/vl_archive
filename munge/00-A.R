8bf53fd8efeedfcebb2f327db65d91196489dc82
Glen C Falk <gfalk@uga.edu>
2021-06-09 08:47:49 -0400

    20210609ValueLineA
    a. add formatR package

--- munge/00-A.R
+++ munge/00-A.R
@@ -10,8 +10,14 @@ started.at <- proc.time()
 dirCheck(mainDir, subDir)
 ################################################################################
 ## Step 00.02: clean dataframes with Janitor                                 ###
+## Use fread on zipped files                        https://tinyurl.com/2nphb6cd
+## You can import a zipped file without unzipping it first.
+## fread can import gz and bz2 files directly, such as mydt <- fread("myfile.gz")
+## If you need to import a zip file, you can unzip it with the unzip system
+## command within fread, using the syntax mydt <- fread(cmd = 'unzip -cq myfile.zip').
 ################################################################################
-
+url <- "http://www3.valueline.com/secure/options/ALLNEW.CSV"
+DT <- data.table::fread(url, fill = TRUE, skip=11, verbose = T)
 ################################################################################
 ## Step 00.99: VERSION HISTORY                                               ###
 ################################################################################
