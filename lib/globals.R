3db7f2426cf7bed16e5a82155b15e828f05f278b
Glen C Falk <gfalk@uga.edu>
2021-07-06 14:50:28 -0400

    20210706ValueLineA
    a. general cleanup

--- lib/globals.R
+++ lib/globals.R
@@ -7,9 +7,14 @@ add.config(
   lastYr = as.numeric(format(Sys.Date(), format="%y")) - 1,
   LastYr4 = as.numeric(format(Sys.Date(), format="%Y"))-1,
   currentAY = as.numeric(paste(as.numeric(format(Sys.Date(), format="%y")) - 1, as.numeric(format(Sys.Date(), format="%y")), sep = "")),
-  header = "Project Template" # header in reports
+  header = "ValueLine" # header in reports
 )
-
+# ------------------------------------------------------------------------------
+# Date Configuration
+# ------------------------------------------------------------------------------
+date_curr_mo   <- as.numeric(format(Sys.Date(), format = "%m"))
+date_curr_yr   <- as.numeric(format(Sys.Date(), format = "%y"))
+date_curr_yr4  <- as.numeric(format(Sys.Date(), format = "%Y"))
 ################################################################################
 ## Repo Package: data management to build centralized metadata repository       ### https://github.com/franapoli/repo
 ## Check existence of directory and create if doesn't exist                     ### https://tinyurl.com/y3adrqwa
