e949f4dc0b26991a68e82f4c56c2c7b4c1dfac9d
UTexas80 <glen.falk@utexas.edu>
2022-11-11 14:31:58 -0500

    20221111ValueLineA
    a. update addins.

--- lib/globals.R
+++ lib/globals.R
@@ -2,23 +2,30 @@
 # Date Formats in R    https://tinyurl.com/r-date-format
 add.config(
   apply.override = FALSE,
-  currentYr = as.numeric(format(Sys.Date(), format="%y")),
-  currentYr4 = as.numeric(format(Sys.Date(), format="%Y")),
-  lastYr = as.numeric(format(Sys.Date(), format="%y")) - 1,
-  LastYr4 = as.numeric(format(Sys.Date(), format="%Y"))-1,
-  currentAY = as.numeric(paste(as.numeric(format(Sys.Date(), format="%y")) - 1, as.numeric(format(Sys.Date(), format="%y")), sep = "")),
+  currentYr = as.numeric(format(Sys.Date(),  format = "%y")),
+  currentYr4 = as.numeric(format(Sys.Date(), format = "%Y")),
+  lastYr = as.numeric(format(Sys.Date(),     format = "%y")) - 1,
+  LastYr4 = as.numeric(format(Sys.Date(),    format = "%Y")) - 1,
+  currentAY = as.numeric(paste(as.numeric(format(Sys.Date(), format = "%y")) - 1, as.numeric(format(Sys.Date(), format = "%y")), sep = "")),
   header = "ValueLine" # header in reports
 )
 # ------------------------------------------------------------------------------
 # Date Configuration
 # ------------------------------------------------------------------------------
-date_curr_mo   <- as.numeric(format(Sys.Date(), format = "%m"))
-date_curr_yr   <- as.numeric(format(Sys.Date(), format = "%y"))
-date_curr_yr4  <- as.numeric(format(Sys.Date(), format = "%Y"))
+date_curr_mo      <- as.numeric(format(Sys.Date(), format = "%m"))
+date_curr_yr      <- as.numeric(format(Sys.Date(), format = "%y"))
+date_curr_yr4     <- as.numeric(format(Sys.Date(), format = "%Y"))
+date_prefix       <- as.character(today(), format = '%Y%m%d')
+# ------------------------------------------------------------------------------
+# Directory Configuration
+# ------------------------------------------------------------------------------
+dir_downloads     <- "C:/Users/glen.falk/Downloads/"
+dir_ms365_top_200 <- "Options/ValueLine/Top200"
+dir_ms365_zip     <- "Options/ValueLine/Zip"
 # ------------------------------------------------------------------------------
 # create global environment to dynamically name data frames                     ### https://tinyurl.com/y3adrqwa ###
 # ------------------------------------------------------------------------------
-g <- globalenv()                             # https://tinyurl.com/r3yrspv   ###
+g                 <- globalenv()             # https://tinyurl.com/r3yrspv   ###
 ################################################################################
 ## Repo Package: data management to build centralized metadata repository       ### https://github.com/franapoli/repo
 ## Check existence of directory and create if doesn't exist                     ### https://tinyurl.com/y3adrqwa
@@ -26,7 +33,7 @@ g <- globalenv()                             # https://tinyurl.com/r3yrspv   ###
 mainDir <- (".")
 subDir  <- ("repo")
 rp_path <- file.path(mainDir, subDir)
-
+# ------------------------------------------------------------------------------
 # Add project specific configuration that can be overridden from load.project()
 add.config(
   apply.override = TRUE
